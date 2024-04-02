dcc35d1ad41a1033bdfa6676776f16714121728d
Rick Yao <abneryaoo@gmail.com>
2024-04-02 05:17:27 +0000

    init

---
+++ lua/community.lua
@@ -0,0 +1,22 @@
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  -- { import = "astrocommunity.pack.lua" },
+  -- { import = "astrocommunity.pack.typescript" },
+  -- { import = "astrocommunity.motion.flash-nvim" },
+  -- { import = "astrocommunity.motion.nvim-surround" },
+  -- { import = "astrocommunity.diagnostics.trouble-nvim" },
+  -- { import = "astrocommunity.colorscheme.everforest" },
+  -- { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+  -- { import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
+  -- { import = "astrocommunity.pack.rust" },
+  -- { import = "astrocommunity.pack.vue" },
+  -- { import = "astrocommunity.project.nvim-spectre" },
+  -- import/override with your plugins folder
+}
