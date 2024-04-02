545377368b81e5f456c60d70d43bce7bc15bb695
Rick Yao <abneryaoo@gmail.com>
2024-04-02 05:27:09 +0000

    feat: add community

--- lua/community.lua
+++ lua/community.lua
@@ -7,16 +7,16 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  -- { import = "astrocommunity.pack.lua" },
-  -- { import = "astrocommunity.pack.typescript" },
-  -- { import = "astrocommunity.motion.flash-nvim" },
-  -- { import = "astrocommunity.motion.nvim-surround" },
-  -- { import = "astrocommunity.diagnostics.trouble-nvim" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
+  { import = "astrocommunity.diagnostics.trouble-nvim" },
   -- { import = "astrocommunity.colorscheme.everforest" },
   -- { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
   -- { import = "astrocommunity.fuzzy-finder.telescope-zoxide" },
-  -- { import = "astrocommunity.pack.rust" },
-  -- { import = "astrocommunity.pack.vue" },
-  -- { import = "astrocommunity.project.nvim-spectre" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.project.nvim-spectre" },
   -- import/override with your plugins folder
 }
