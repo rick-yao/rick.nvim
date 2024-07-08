14854aa3c1b6b166ccd77c7342ec050ffe556365
Rick Yao <abneryaoo@gmail.com>
2024-07-08 10:13:02 +0800

    feat: update supermaven keymap

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -2,6 +2,11 @@ return {
   "supermaven-inc/supermaven-nvim",
   config = function()
     require("supermaven-nvim").setup {
+      keymaps = {
+        accept_suggestion = "<M-l>",
+        clear_suggestion = "<M-]>",
+        accept_word = "<M-j>",
+      },
       log_level = "off", -- set to "off" to disable logging completely
     }
   end,
