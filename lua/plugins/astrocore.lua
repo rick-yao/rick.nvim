d2cf4dfc619c2bbc3291bcd964818f97422e5349
Rick Yao <abneryaoo@gmail.com>
2024-07-19 13:46:54 +0800

    fix: clipboard not working on linux

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -27,6 +27,7 @@ return {
     -- vim options can be configured here
     options = {
       opt = { -- vim.opt.<key>
+        clipboard = "unnamedplus",
         relativenumber = true, -- sets vim.opt.relativenumber
         number = true, -- sets vim.opt.number
         spell = false, -- sets vim.opt.spell
