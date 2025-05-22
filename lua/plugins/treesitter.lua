c80aa00c0eb43a9258948aaa7eae1568b787b49f
Rick Yao <abneryaoo@gmail.com>
2025-05-22 13:21:40 +0800

    feat: upgrade to v5

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,12 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- Customize Treesitter
 
 ---@type LazySpec
 return {
   "nvim-treesitter/nvim-treesitter",
-  opts = function(_, opts)
-    -- add more things to the ensure_installed table protecting against community packs modifying it
-    opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
+  opts = {
+    ensure_installed = {
       "json",
       "javascript",
       "typescript",
@@ -24,6 +23,6 @@ return {
       "dockerfile",
       "gitignore",
       -- add more arguments for adding more treesitter parsers
-    })
-  end,
+    },
+  },
 }
